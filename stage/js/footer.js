801b42ab315257b7b42adfc4d8d77066a840a6ef
mohamed17717 <root@localhost.localdomain>
2019-04-06 21:34:25 +0200

    initate the project

---
+++ stage/js/footer.js
@@ -0,0 +1,20 @@
+// functions
+function generateHexaInRandom(){
+  // generate div to five him specific font size max 1px
+  // put other elm with .hexa
+  // give random opacity to this div
+  // giv this div random top left positions
+  // give first div random timeout to disppear
+  // there is 2 colors for those hexa
+}
+
+
+// events
+document.querySelector('form').onsubmit = function(e){
+  return false;
+}
+
+document.addEventListener('DOMContentLoaded', (e)=>{
+  document.body.style.minHeight = window.innerHeight+'px';
+})
+
